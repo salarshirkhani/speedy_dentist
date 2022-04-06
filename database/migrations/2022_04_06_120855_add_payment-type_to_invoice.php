0a98ca24ddb8f1273151c9a8dbad921b08fc55ad
salarshirkhani <salarshirkhany16@gmail.com>
2022-04-06 16:41:57 +0430

    enhance payment type

---
+++ database/migrations/2022_04_06_120855_add_payment-type_to_invoice.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPaymentTypeToInvoice extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->string('payment_type')->nullable();
+            $table->string('DOS')->nullable();
+            $table->unsignedBigInteger('provider')->nullable();
+            $table->foreign('provider')->references('id')->on('providers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('invoice', function (Blueprint $table) {
+            //
+        });
+    }
+}
